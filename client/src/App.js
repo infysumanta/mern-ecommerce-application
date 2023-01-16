d4a53ff82ad566ed215200e38546832fefec1523
Sumanta Kabiraj <sumantablog@gmail.com>
2023-01-16 12:55:35 +0530

    client layouts setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+import { NotFound } from "./components/NotFound";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/cart" component={Cart} />
+          <Route path="/" exact component={Home} />
+          <Route path="/*" component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
