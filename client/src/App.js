9a550060cc4100825cd4224a3734ce951e51d22a
Sumanta Kabiraj <sumantablog@gmail.com>
2023-01-20 13:13:22 +0530

    setup products and dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,9 @@ import CheckOutSuccess from "./components/CheckOutSuccess";
 import Dashboard from "./components/admin/Dashboard";
 import Products from "./components/admin/Products";
 import Summary from "./components/admin/Summary";
+import Orders from "./components/admin/Orders";
+import Users from "./components/admin/Users";
+import CreateProduct from "./components/admin/CreateProduct";
 
 function App() {
   return (
@@ -23,8 +26,12 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/checkout-success" element={<CheckOutSuccess />} />
           <Route path="/admin" element={<Dashboard />}>
-            <Route path="products" element={<Products />} />
             <Route path="summary" element={<Summary />} />
+            <Route path="products" element={<Products />}>
+              <Route path="create-product" element={<CreateProduct />} />
+            </Route>
+            <Route path="orders" element={<Orders />} />
+            <Route path="users" element={<Users />} />
           </Route>
           <Route path="/*" element={<NotFound />} />
         </Routes>
